12445c547a9188b07ba3549f235ad60da65564c4
waterbender <pask.eugene@gmail.com>
2015-07-16 17:29:14 +0300

    Initial Commit

---
+++ GuruCrafter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GuruCrafter
+//
+//  Created by  ZHEKA on 16.07.15.
+//  Copyright (c) 2015 Pasko Eugene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
